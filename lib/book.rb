3f9132e0cd1ecf52e8c3db13683c0772474cec9b
Samantha brake <sammy.brake@gmail.com>
2018-09-14 02:06:07 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,12 @@ class Book
     @author = author 
   end 
   
-  def page_count=(pag)
+  def page_count=(page_count) 
+    @page_count = page_count 
+  end 
+  
+  def page_count 
+    @page_count 
+  end 
   
 end 
